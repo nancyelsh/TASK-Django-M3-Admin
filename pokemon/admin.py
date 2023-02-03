8d7112645da59d47295f9967cbf3a559b6c9e387
Nancy El-Sharkawi <51517316+nancyelsh@users.noreply.github.com>
2023-02-03 23:34:57 +0300

    finished task

--- pokemon/admin.py
+++ pokemon/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Pokemon
+
+class PokemonAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'hp', 'active')
+    list_filter = ('active',)
+    list_display_links = ('id', 'name')
+    fieldsets = (
+        (None, {"fields": ("name", "type", "hp", "active")}),
+        (
+            "Localizations",
+            {"classes": ("collapse",),
+             "fields": ("name_fr", "name_jp", "name_ar")},
+        ),
+    )
+
+
+
 
 # Register your models here.
+admin.site.register(Pokemon, PokemonAdmin)
--- pokemon/models.py
+++ pokemon/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
+class Pokemon(models.Model):
+
+    class PokemonType(models.TextChoices):
+        WATER = 'WA'
+        GRASS = 'GR'
+        GHOST = 'GH'
+        STEEL = 'ST'
+        FAIRY = 'FA'
+        
+    name = models.CharField(max_length=30)
+    type = models.CharField(choices=PokemonType.choices, max_length=2)
+    hp = models.PositiveIntegerField(validators=[MinValueValidator(50), MaxValueValidator(350)])
+    active = models.BooleanField(default=True)
+    name_fr = models.CharField(max_length=30, default="", blank=True)
+    name_jp = models.CharField(max_length=30, default="", blank=True)
+    name_ar = models.CharField(max_length=30, default="", blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
